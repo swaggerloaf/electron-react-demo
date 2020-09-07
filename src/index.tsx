976bc5b2f7fa054331b5d32a44d2fa83cf60541c
swaggerloaf <swaggerloaf@gmail.com>
2020-09-07 19:12:04 -0400

    updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { FaStar } from "react-icons/fa";
+import { FaStar, FaRegStar } from "react-icons/fa";
 // import "./styles.scss";
 // import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
-const Button = () => (
-  <button className="render_button">
-    <FaStar />
-    <span>Add</span>
-  </button>
-);
+function Button(props: any) {
+  return <button className="render_button">{props.children}</button>;
+}
 
-const domElement = document.getElementById("root");
-ReactDOM.render(<Button />, domElement);
+function App() {
+  return (
+    <div>
+      <Button>
+        <FaStar /> Add
+      </Button>
+      <Button>
+        <FaRegStar /> Subtract
+      </Button>
+    </div>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
